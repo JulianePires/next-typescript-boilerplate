f999d215f405b014ffd3dff8caa2837b1ab15d8c
JulianePires <mpiresjuliane@outlook.com>
2021-07-10 23:07:52 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withImages = require("next-images");
+
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    ...defaultConfig,
+
+    rewrites: async function () {
+      return [];
+    },
+
+    webpack: function (config) {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: "raw-loader",
+      });
+      return config;
+    },
+    ...withImages(defaultConfig),
+  };
+};
